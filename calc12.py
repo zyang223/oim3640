8b1da8cdd75e54dc277c3e64b229213cc1e23792
zyang223 <122936587+zyang223@users.noreply.github.com>
2023-02-02 11:51:24 +0800

    Update calc12.py

--- calc12.py
+++ calc12.py
@@ -1,29 +1,22 @@
 ###############################
-#Excercise 01 Feb 1,2023
+# Excercise 01 Feb 1,2023
 import math
-n=3.1415
+
+n = 3.1415
 print(round(n))
-print(round(n,2))
+print(round(n, 2))
 print(math.log10(100))
-#round
-print(min(3,4,5,6))
-print(min(-3,-4,-5,-6))
-#min
+# round
+print(min(3, 4, 5, 6))
+print(min(-3, -4, -5, -6))
+# min
 print(ord("a"))
 print(ord("$"))
-#ord
+# ord
 print(chr(97))
 print(chr(36))
 ########################################################
-#Exercise 02 (trunc,)
-print(math.trunc(10/3))
+# Exercise 02 (trunc,)
+print(math.trunc(10 / 3))
 print(math.sqrt(4))
 print(math.trunc(math.sqrt(3)))
-
-
-
-    
-
-
-
-
