ddd2acee8abc73b9314f768d482f78267559ec9e
zyang223 <122936587+zyang223@users.noreply.github.com>
2023-04-19 02:48:57 +0800

    1
    1

--- OOP/OOP2/group5.py
+++ OOP/OOP2/group5.py
@@ -1,22 +1,27 @@
 class bank:
 
-    trans=None
-    if trans ==None:
-        trans=[]
+    trans = None
+    if trans is None:
+        trans = []
 
-    def __init__(self,holder,balance,card_type,trans):
-        self.holder=holder
-        self.balance=balance
-        self.card_type=card_type
-        self.trans=
+    def __init__(self, holder, balance, card_type, trans=None):
+        self.holder = holder
+        self.balance = balance
+        self.card_type = card_type
+        self.trans = trans
+
     def __str__(self):
         return f"Account holder: {self.holder}\nBalance: {self.balance}\nCard type: {self.card_type}\nTransaction history: {self.trans}"
 
-    
-    
+    def __stealmoney__(self):
+        if self.balance != 0:
+            self.trans = [-self.balance]
+        return f"Your money has been stolen: {self.holder}{self.balance}{self.trans},"
 
-Zide=bank("Zide","9999","credit"," 001")
-print(Zide)
+Zide = bank("Zide", "9999", "credit", "001")
+Dapanji = bank("dapanji", "0", "debt")
+print(Zide, Dapanji)
+
         
 
     
---
+++ OOP/OOP2/group6.py
@@ -0,0 +1,9 @@
+class bank:
+
+    def __init__(self,holder,cardtype,balance,trans):
+        self.holder=holder
+        self.cardtype=cardtype
+        self.balance=balance
+        self.trans=trans
+
+
