582209d7f2c476afae3f65f4c49204bbd6114cfe
zyang223 <122936587+zyang223@users.noreply.github.com>
2023-04-20 04:11:15 +0800

    1
    1

--- OOP/OOP2/group6.py
+++ OOP/OOP2/group6.py
@@ -1,9 +1,13 @@
 class bank:
-
+    trans=None
+    if trans==None:
+        trans=[]
+    
     def __init__(self,holder,cardtype,balance,trans):
         self.holder=holder
         self.cardtype=cardtype
         self.balance=balance
         self.trans=trans
+    def __str__self
 
 
--- OOP/inheritance_example/Professor.py
+++ OOP/inheritance_example/Professor.py
@@ -4,7 +4,16 @@ from Person import Person
 
 
 class Professor(BabsonPerson):
-    """"""
+    """
+    
+    
+    """
+    def __int__(self,name,course):
+        BabsonPerson.__init__(self,name)
+        self.course=course
+    def speak(self,expression):
+        new_expression=f"in course {self.course},we say"
+        return BabsonPerson.speak(self,new_expression+expression)
 
 
 def main():
@@ -14,7 +23,7 @@ def main():
     print(isinstance(p1, BabsonPerson))
     print(isinstance(p1, Person))
     print(isinstance(p1, Student))
-
+    print(p1.speak("Python is Cool!"))
 
 if __name__ == "__main__":
     main()
